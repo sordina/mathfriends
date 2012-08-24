9c56ec2bb59bb0ebb4f94f9a73fdff7f9a66f691
Lyndon Maydwell <maydwell@gmail.com>
2012-08-24 15:49:00 +0800

    Live push for facebook

--- facebook.js
+++ facebook.js
@@ -1,4 +1,4 @@
 
 // Meteor.accounts.facebook.config("312400802192165", "http://mathfriends.meteor.com", {})
 //
-Meteor.accounts.facebook.config("312400802192165", "http://dev.mathfriends.meteor.com:3000", {})
+Meteor.accounts.facebook.config("312400802192165", "http://mathfriends.meteor.com", {})
